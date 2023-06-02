39b2ceff7e0730f9f930ea9b4b524f5b39ee1f52
Serhii <cherkashenserg@gmail.com>
2023-06-02 13:16:09 +0300

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import File from './file/File';
+import { useTranslation, Trans } from 'react-i18next';
+
+
+const lngs = {
+  en: { nativeName: 'English' },
+  de: { nativeName: 'Deutsch' }
+};
 
 function App() {
+
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="App">
       <File />
+      <div>
+        {Object.keys(lngs).map((lng) => (
+          <button key={lng} style={{
+            fontWeight: i18n.resolvedLanguage === lng ?
+              'bold' : 'normal'
+          }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+            {lngs[lng].nativeName}
+          </button>
+        ))}
+      </div>
+      <p>
+        <Trans i18nKey="description.part1">
+          Car Edit <code>src/App.js</code> car and save to reload.
+          car
+        </Trans>
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {t('description.part2')}
+      </a>
     </div>
   );
 }
